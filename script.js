831ea2a690548f8e4dea54c6ce706c4a935bcb0f
Dharam8218 <101041058+Dharam8218@users.noreply.github.com>
2025-02-13 12:38:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
 function makeid(l) {
   // write your code here
+	var res="";
+	var char_list="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+	for(var i=0;i<l;i++){
+		res= res + char_list.charAt(
+			Math.floor(
+				Math.random()*char_list.length
+				
+			)
+		)
+
+		
+	}
+
+	return res;
 }
 
 // Do not change the code below.
 const l = prompt("Enter a number.");
 alert(makeid(l));
+
